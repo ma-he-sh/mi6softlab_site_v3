ff24ec36b49258ed3ffb505a2da60c54391dabfc
Mahesh Ranaweera <mi6softlab@gmail.com>
2018-01-08 15:34:04 -0500

    finish index

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,23 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'mi6softlab' });
+});
+
+router.get('/design', function(req, res, next){
+  res.render('design', { title: 'Design' });
+});
+
+router.get('/dev', function(req, res, next){
+  res.render('dev', { title: 'Dev' });
+});
+
+router.get('/blog', function(req, res, next){
+  res.render('blog', { title: 'Blog' });
+});
+
+router.get('/about', function(req, res, next){
+  res.render('about', { title: 'About' });
 });
 
 module.exports = router;
