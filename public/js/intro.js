ecf685e4c2b81096de0195f223cb1840d6ca598e
Mahesh Ranaweera <mi6softlab@gmail.com>
2018-01-08 23:04:58 -0500

    adde muuri

---
+++ public/js/intro.js
@@ -0,0 +1,23 @@
+/**
+ * Intro to the website
+ */
+var typed = new Typed('#description', {
+    strings: [
+            '<b>Hello</b>',
+            'Welcome to <b>Dark Side</b>',
+            '😂',
+            'Welcome to my website',
+            'I am <b>Mahesh Ranaweera</b>',
+            'Designer and a Soft. Engineer',
+            'You can find my graphic designs,',
+            'things that I have done,',
+            'read my blog to see what I am doing',
+            'or my <b>resume</b>',
+            '<b>May the force be with you</b>'
+        ],
+    typeSpeed: 60,
+    backSpeed: 60,
+    fadeOut: true,
+    loop: true,
+    cursorChar: ''
+  });
