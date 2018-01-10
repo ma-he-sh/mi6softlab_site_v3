515b80c54e1016198389764db0015ac9b88aa461
Mahesh Ranaweera <mi6softlab@gmail.com>
2018-01-10 00:13:24 -0500

    dev section main coding done

--- routes/index.js
+++ routes/index.js
@@ -4,31 +4,38 @@ var jsonfile = require('jsonfile');
 var fs = require('fs');
 
 var json_f = './app/data.json';
+var jsonData = jsonfile.readFileSync(json_f)
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
   res.render('index', { title: 'mi6softlab' });
 });
 
+/* GET design page. */
 router.get('/design', function (req, res, next) {
-  var jsonData = jsonfile.readFileSync(json_f)
-
-  //console.log(jsonData.data.design);
-  
-  res.render('design', { 
+  res.render('design', {
     title: 'Design',
     content: jsonData.data.design
   });
 });
 
+/* GET dev page. */
 router.get('/dev', function (req, res, next) {
-  res.render('dev', { title: 'Dev' });
+  res.render('dev', { 
+    title: 'Dev',
+    content: jsonData.data.dev
+  });
 });
 
+/* GET blog page. */
 router.get('/blog', function (req, res, next) {
-  res.render('blog', { title: 'Blog' });
+  res.render('blog', { 
+    title: 'Blog',
+    content: jsonData.data.blog 
+  });
 });
 
+/* GET about page. */
 router.get('/about', function (req, res, next) {
   res.render('about', { title: 'About' });
 });
