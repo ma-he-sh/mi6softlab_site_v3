9e05ae34ca68c3ba85a00cc3f8917f196306310e
Mahesh Ranaweera <mi6softlab@gmail.com>
2018-01-13 15:38:52 -0500

    fix-some issues

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', '/assets/favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -26,8 +26,10 @@ app.use('/', index);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+  //err.status = 404;
+  //next(err);
+
+  res.redirect('/');
 });
 
 // error handler
